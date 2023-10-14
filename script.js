f9a1c1927862c69616c3200ac3fe7275f1984257
Michael Ha <77702012+Guacamoley@users.noreply.github.com>
2023-10-13 19:06:11 -0500

    Implement basic arithmetic functionality

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const display = document.getElementById("result");
 const buttons = document.querySelectorAll("button");
-let firstNumber = 0;
-let secondNUmber = 0;
+let currentDisplayValue = "";
+let numHolder = "";
 let operator = "";
 
 function add(a, b) {
@@ -21,14 +21,16 @@ function divide(a, b) {
 }
 
 function operate(operator, num1, num2) {
+  num1 = parseInt(num1, 10);
+  num2 = parseInt(num2, 10);
   switch (operator) {
     case "+":
       return add(num1, num2);
     case "-":
       return subtract(num1, num2);
-    case "*":
+    case "×":
       return multiply(num1, num2);
-    case "/":
+    case "÷":
       return divide(num1, num2);
     default:
       return "Invalid operator";
@@ -39,27 +41,42 @@ buttons.forEach(function (button) {
   button.addEventListener("click", function () {
     switch (true) {
       case button.classList.contains("digits"):
-        populateDisplay(button);
+        populateDisplay(button.textContent);
+        currentDisplayValue += button.textContent;
         break;
 
       case button.classList.contains("all-clear"):
         clearDisplay();
-        firstNumber = 0;
-        secondNumber = 0;
+        currentDisplayValue = "";
         break;
 
       case button.className === "clear":
         clearDisplay();
         break;
 
+      case button.className === "operators":
+        populateDisplay(button.textContent);
+        numHolder = currentDisplayValue;
+        operator = button.textContent;
+        currentDisplayValue = "";
+        clearDisplay();
+        break;
+
+      case button.className === "equals":
+        clearDisplay();
+        total = operate(operator, numHolder, currentDisplayValue);
+        currentDisplayValue = "";
+        numHolder = "";
+        operator = "";
+        populateDisplay(total);
       default:
         return;
     }
   });
 });
 
-function populateDisplay(button) {
-  display.value += button.textContent;
+function populateDisplay(textToDisplay) {
+  display.value += textToDisplay;
 }
 
 function clearDisplay() {
